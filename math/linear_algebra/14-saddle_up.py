6df9e41a1c03c873605b4ae5b10451eece799a96
Natinael-1 <n.borana@alustudent.com>
2026-09-15 14:29:55 -0700

    editted file

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,5 +2,7 @@
 """Module that performs matrix multiplication using numpy."""
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
+    """Performs matrix multiplication of two numpy arrays."""
     return np.matmul(mat1, mat2)
